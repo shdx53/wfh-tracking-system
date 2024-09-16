0c88c55d6af7f91eea0625d6ce59c3e9cbf6c4c6
Shi Da <tanshida.53@gmail.com>
2024-09-16 21:37:41 +0800

    Install shadcn tabs component and useMediaQuery hook

--- components/ui/calendar.jsx
+++ components/ui/calendar.jsx
@@ -14,7 +14,6 @@ function Calendar({
 }) {
   return (
     <DayPicker
-      captionLayout="dropdown"
       showOutsideDays={showOutsideDays}
       className={cn("p-3", className)}
       classNames={{
@@ -30,13 +29,14 @@ function Calendar({
         nav_button_previous: "absolute left-1",
         nav_button_next: "absolute right-1",
         table: "w-full border-collapse",
-        head_row: "flex justify-between my-4 sm:mt-6 sm:mb-0",
-        head_cell: "text-muted-foreground rounded-md w-10 font-normal text-sm",
-        row: "flex w-full my-4 sm:my-6 justify-between",
-        cell: "h-10 w-10 text-center text-sm p-0 relative [&:has([aria-selected].day-range-end)]:rounded-r-md [&:has([aria-selected].day-outside)]:bg-accent/50 [&:has([aria-selected])]:bg-accent first:[&:has([aria-selected])]:rounded-l-md last:[&:has([aria-selected])]:rounded-r-md focus-within:relative focus-within:z-20",
+        head_row: "flex justify-between my-4 sm:mt-6 sm:mb-0 lg:mt-8",
+        head_cell:
+          "text-muted-foreground rounded-md w-10 font-normal text-sm lg:text-base",
+        row: "flex w-full my-4 sm:my-6 lg:my-8 justify-between last:mb-0",
+        cell: "h-10 w-10 lg:h-11 lg:w-11 text-center text-sm p-0 relative [&:has([aria-selected].day-range-end)]:rounded-r-md [&:has([aria-selected].day-outside)]:bg-accent/50 [&:has([aria-selected])]:bg-accent first:[&:has([aria-selected])]:rounded-l-md last:[&:has([aria-selected])]:rounded-r-md focus-within:relative focus-within:z-20",
         day: cn(
           buttonVariants({ variant: "ghost" }),
-          "h-10 w-10 p-0 font-normal aria-selected:opacity-100 text-base",
+          "h-10 w-10 lg:w-11 lg:w-11 p-0 font-normal aria-selected:opacity-100 text-base lg:text-lg",
         ),
         day_range_end: "day-range-end",
         day_selected:
