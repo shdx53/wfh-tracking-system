0eb98424d3a0e2464cd3904b83a6746c3e52f904
Shi Da <tanshida.53@gmail.com>
2024-09-21 02:46:16 +0800

    View Overall Schedule: Debug production errors

--- app/schedule/overall/page.jsx
+++ app/schedule/overall/page.jsx
@@ -21,13 +21,9 @@ export default function OverallSchedule() {
 
   // Format date for display on the UI
   const dateObj = new Date(date);
-  const formattedDate = dateObj.toLocaleDateString("en-US", {
-    weekday: "short", // e.g., Mon
-    month: "short", // e.g., Sep
-    day: "2-digit", // e.g., 16
-    year: "numeric", // e.g., 2024
-  });
-
+  const dateParts = dateObj.toDateString().split(" ");
+  const formattedDate = `${dateParts[0]}, ${dateParts[1]} ${dateParts[2]}, ${dateParts[3]}`;
+  
   function renderTabContent() {
     return (
       <>
