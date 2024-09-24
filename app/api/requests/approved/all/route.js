2f9fe2a9a9932ad94092f312b0ed02b5a6a5ee8a
ginnnn-e <144206861+ginnnn-e@users.noreply.github.com>
2024-09-24 18:32:39 +0800

    View Overall Schedule: now includes staff in office

--- app/api/requests/approved/all/route.js
+++ app/api/requests/approved/all/route.js
@@ -3,7 +3,7 @@ import { NextResponse } from "next/server";
 
 // API Endpoint: api/requests/approved/all
 
-// Fetch ALL approved requests from the Arrangement table
+// Fetch ALL approved requests from the Arrangement table - and also who is in office
 export async function GET(request) {
   try {
     // Establish the connection using the pool
@@ -14,7 +14,10 @@ export async function GET(request) {
 
     // Execute the query
     const [data] = await conn.query(
-      "SELECT Staff_ID, Start_Date, Shift_Type FROM Arrangement WHERE Request_Status = 'approved'"
+      `SELECT e.Staff_ID, COALESCE(a.Start_Date, NULL) AS Start_Date, COALESCE(a.Shift_Type, NULL) AS Shift_Type, Request_Status
+      FROM Arrangement a
+      RIGHT JOIN Employee e
+      ON Request_Status = 'approved' AND a.Staff_ID = e.Staff_ID;`
     );
 
     // Release the connection back to the pool
