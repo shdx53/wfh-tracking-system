b41902b5ae14e8f647a50352db2119230818a77c
Shi Da <tanshida.53@gmail.com>
2024-09-29 16:46:07 +0800

    Apply for Arrangement: Add Suspense

--- app/arrangements/new/page.jsx
+++ app/arrangements/new/page.jsx
@@ -1,3 +1,6 @@
+// Library
+import { Suspense } from "react";
+
 // Component
 import ArrangementForm from "@/components/arrangements/form";
 
@@ -6,12 +9,14 @@ export default function NewArrangement() {
     <div className="mx-auto my-8 flex max-w-lg justify-between gap-8 rounded-lg border p-6 sm:p-8 md:max-w-none">
       <div className="w-full space-y-6 md:w-1/2">
         <h1 className="text-xl font-bold">Create new arrangement</h1>
-        <ArrangementForm />
+        <Suspense>
+          <ArrangementForm />
+        </Suspense>
       </div>
       <img
         src="/images/nubelson-fernandes-Y9V-pKSy0fw-unsplash.jpg"
         alt="A white desk featuring a laptop and a chair, creating a clean and modern workspace environment."
-        className="hidden w-1/2 md:block object-cover"
+        className="hidden w-1/2 object-cover md:block"
       />
     </div>
   );
