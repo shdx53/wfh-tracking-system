fd06d885ebc267d0233a9f20b03fb94d659a07f6
Shi Da <tanshida.53@gmail.com>
2024-10-06 16:03:57 +0800

    Change API routes

--- app/lib/arrangements/fetch-arrangements.js
+++ app/lib/arrangements/fetch-arrangements.js
@@ -1,11 +1,23 @@
-export async function fetchArrangements(selectedDate) {
-  const res = await fetch(
-    `${process.env.NEXT_PUBLIC_BASE_URL}/api/requests?startDate=${selectedDate}`,
-  );
+export async function fetchArrangements(selectedDate, arrangementID) {
+  let res;
+
+  // Fetch arrangements by Start_Date
+  if (selectedDate) {
+    res = await fetch(
+      `${process.env.NEXT_PUBLIC_BASE_URL}/api/arrangements?startDate=${selectedDate}`,
+    );
+  }
+
+  // Fetch arrangements by Arrangement_ID
+  if (arrangementID) {
+    res = await fetch(
+      `${process.env.NEXT_PUBLIC_BASE_URL}/api/arrangements/pending/recurring/${arrangementID}`,
+    );
+  }
 
   if (!res.ok) {
-    throw new Error('Network response was not ok');
+    throw new Error("Network response was not ok");
   }
-  
+
   return res.json();
 }
