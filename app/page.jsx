0387909659e66e0649914e79da430446c024553f
je.rnic <yeojernic@gmail.com>
2024-10-07 16:55:52 +0800

    Login to WFH Tracking System
    not in picture: 12hours worth of scrolling through nextAuth documentation just to throw it out the window

--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,17 @@
+import {RegisterLink, LoginLink, LogoutLink} from "@kinde-oss/kinde-auth-nextjs/components";
+
+
 export default function Home() {
   return (
     <>
       <div>Home page</div>
+
+      <div className="flex flex-col items-center space-y-4">
+        <LoginLink>Sign in</LoginLink>
+        <RegisterLink>Sign up</RegisterLink>
+        <LogoutLink>Log out</LogoutLink>
+
+      </div>
     </>
   );
 }
