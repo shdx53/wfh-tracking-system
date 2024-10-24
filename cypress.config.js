22a29cc22f863629b97f2e33b8b841a7d6d57c5c
Swee Long <61006436+ongsweelong@users.noreply.github.com>
2024-10-24 15:25:42 +0800

    End to End Testing: Solving Coverage Report

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,27 @@
 import { defineConfig } from "cypress";
+import codeCoverage from '@cypress/code-coverage/task';
 
 export default defineConfig({
+  projectId: "5cw9tf",
+
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      // Integrating Cypress code coverage plugin for E2E tests
+      codeCoverage(on, config);
+      return config;
     },
+    baseUrl: 'https://wfh-tracking-system.vercel.app/', // Adjust this based on your app
+  },
+
+  component: {
+    devServer: {
+      framework: "next",
+      bundler: "webpack",
+    },
+    setupNodeEvents(on, config) {
+      // Integrating Cypress code coverage plugin for component tests
+      codeCoverage(on, config);
+      return config;
+    }
   },
 });
