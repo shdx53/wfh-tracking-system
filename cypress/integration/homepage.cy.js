877134a8eaad30abf788874f33bf2beb695fcf0d
Swee Long <61006436+ongsweelong@users.noreply.github.com>
2024-10-25 18:05:43 +0800

    End to End Testing: Removed unnecessary test files

--- cypress/integration/homepage.cy.js
+++
@@ -1,6 +0,0 @@
-describe('Basic test', () => {
-    it('Visits the home page', () => {
-      cy.visit('/'); // Visit the home page
-      cy.contains('Welcome'); // Check for a welcome message
-    });
-  });
