9c29a339581a7ffe6823e25cae17f8e242cbf0f6
ginnnn-e <144206861+ginnnn-e@users.noreply.github.com>
2024-10-27 00:34:24 +0800

    Cancel (US14), Withdraw (US13): Cancel function works now, requests are deleted upon cancellation. Added email sending to Reporting Manager whenever a request is withdrawn.

--- app/actions/arrangements/requests/manage-arrangement.js
+++ app/actions/arrangements/requests/manage-arrangement.js
@@ -91,7 +91,30 @@ export async function manageArrangement(formData) {
                 Start Dates: ${startDates}\n
                 Reason: ${reason}\n\n
                 Thank you. \n\n`;
-              }       
+              }
+              
+              // (for Reporting Manager email) Notification to staff's reporting manager (only for withdrawal of entire arrangement)
+
+              // Get staff's reporting manager ID
+              const [reportingManagerID_query] = await conn.query("SELECT Reporting_Manager FROM Employee WHERE Staff_ID = ?", [staffID]);
+              const reportingManagerID = reportingManagerID_query[0]['Reporting_Manager'];
+
+              // Get staff's reporting manager email
+              const [reportingManagerEmail_query] = await conn.query("SELECT Email FROM Employee WHERE Staff_ID = ?", [reportingManagerID]);
+              var reportingManagerEmail = reportingManagerEmail_query[0]['Email'];
+
+              // Prepare email content for Reporting Manager
+              // for withdrawal of entire arrangement
+              if (action === "Withdraw entire arrangement") {
+                // Prepare email content - reason is not optional
+                var subject1 = "WFH Request " + requestStatus[action];
+                var body1 = `Dear Reporting Manager (${reportingManagerEmail}),\n\n
+                An entire recurring work-from-home arrangement was ${requestStatus[action]}:\n\n
+                Staff ID: ${staffID}\n
+                Start Dates: ${startDates}\n
+                Reason: ${reason}\n\n
+                Thank you. \n\n`;
+              } 
             } 
             
             else {
@@ -106,6 +129,21 @@ export async function manageArrangement(formData) {
               const startDate = startDate_query[0]['Start_Date'];
               outcome[startDate] = { "action": requestStatus[action],
                                       "reason": reason };
+              
+              // (for Reporting Manager email) Get reporting manager ID if withdraw single arrangement
+              if (action === "Withdraw this specific arrangement only") {
+                // Get staff ID
+                const [staffID_query] = await conn.query("SELECT Staff_ID FROM Arrangement WHERE Arrangement_ID = ?", [arrangementID]);
+                const staffID = staffID_query[0]['Staff_ID'];
+
+                // Get staff's reporting manager ID
+                const [reportingManagerID_query] = await conn.query("SELECT Reporting_Manager FROM Employee WHERE Staff_ID = ?", [staffID]);
+                const reportingManagerID = reportingManagerID_query[0]['Reporting_Manager'];
+
+                // Get staff's reporting manager email
+                const [reportingManagerEmail_query] = await conn.query("SELECT Email FROM Employee WHERE Staff_ID = ?", [reportingManagerID]);
+                var reportingManagerEmail = reportingManagerEmail_query[0]['Email'];
+              }                          
             }
           } 
 
@@ -141,6 +179,10 @@ export async function manageArrangement(formData) {
             // (for email) Get outcome details of the single arrangement
             outcome[startDate] = { "action": requestStatus[action],
                                       "reason": reason };
+
+            // Cancelling requests stops here, does not send any email
+            console.log('Arrangement ID:', arrangementID, 'Cancel success')
+
           }
         // } 
         // catch (error) {
@@ -151,69 +193,82 @@ export async function manageArrangement(formData) {
       }
     }
 
-    // Notification to staff (for other types of arrangements)
-    // Get staff email, start date
-    const [staffID_query] = await conn.query("SELECT Staff_ID FROM Arrangement WHERE Arrangement_ID = ?", [arrangementID]);
-    const staffID = staffID_query[0]['Staff_ID'];
+    // Send email for arrangements that are non-cancel
+    if (action !== "Cancel") {
+      // Notification to staff (for other types of arrangements)
+      // Get staff email, start date
+      const [staffID_query] = await conn.query("SELECT Staff_ID FROM Arrangement WHERE Arrangement_ID = ?", [arrangementID]);
+      const staffID = staffID_query[0]['Staff_ID'];
 
-    const [staffEmail_query] = await conn.query("SELECT Email FROM Employee WHERE Staff_ID = ?", [staffID]);
-    var staffEmail = staffEmail_query[0]['Email'];
+      const [staffEmail_query] = await conn.query("SELECT Email FROM Employee WHERE Staff_ID = ?", [staffID]);
+      var staffEmail = staffEmail_query[0]['Email'];
 
-    // Email content for recurring arrangements
-    if (Object.keys(outcome).length > 1) { // there will be MORE THAN 1 start date in 'outcome'
-      var details = ``;
-      for (const key in outcome) {
-        const date = key; // The date string as the key
-        const { action, reason } = outcome[key]; // Destructure to get action and reason
-        details += 
-      `\nStart Date: ${date}\n
-      Outcome: ${action}\n
-      Reason: ${reason || "N/A"}\n\n`;
-      }
-
-      // Prepare email content for cancelled requests
-      if (details.includes("cancelled")) {
-        var subject = "WFH Request cancelled";
-        var body = `Dear Staff,\n\n
-        The following recurring work-from-home arrangement dates have been cancelled:\n\n
-        Staff ID: ${staffID}\n
-        ${details}\n
-        Thank you. \n\n`;
-      }
-      
-      // Prepare email content for other requests
-      else {
+      // Email content for recurring arrangements
+      if (Object.keys(outcome).length > 1) { // there will be MORE THAN 1 start date in 'outcome'
+        var details = ``;
+        for (const key in outcome) {
+          const date = key; // The date string as the key
+          const { action, reason } = outcome[key]; // Destructure to get action and reason
+          details += 
+        `\nStart Date: ${date}\n
+        Outcome: ${action}\n
+        Reason: ${reason || "N/A"}\n\n`;
+        } 
+        // Prepare email content
         var subject = "WFH Request outcome";
         var body = `Dear Staff,\n\n
         Please see the outcomes of your recurring work-from-home arrangement dates:\n\n
         Staff ID: ${staffID}\n
         ${details}\n
         Thank you. \n\n`;
-      } 
-    }
-    // Email content for ad-hoc arrangements
-    else if (Object.keys(outcome).length === 1) { // there will be ONLY 1 start date in 'outcome'
-      for (const key in outcome) {
-        var date = key; // The date string as the key
-        var { action, reason } = outcome[key]; // Destructure to get action and reason
       }
-      // Prepare email content
-      var subject = "WFH Request " + action;
-      var body = `Dear Staff,\n\n
-      A work-from-home arrangement was ${action}:\n\n
-      Staff ID: ${staffID}\n
-      Start Date: ${date}\n
-      Reason: ${reason || "N/A"}\n\n
-      Thank you. \n\n`;
+
+      // Email content for ad-hoc arrangements
+      else if (Object.keys(outcome).length === 1) { // there will be ONLY 1 start date in 'outcome'
+        for (const key in outcome) {
+          var date = key; // The date string as the key
+          var { action, reason } = outcome[key]; // Destructure to get action and reason
+        }
+        // Prepare email content
+        var subject = "WFH Request " + action;
+        var body = `Dear Staff,\n\n
+        A work-from-home arrangement was ${action}:\n\n
+        Staff ID: ${staffID}\n
+        Start Date: ${date}\n
+        Reason: ${reason || "N/A"}\n\n
+        Thank you. \n\n`;
+
+        // (for Reporting Manager Email) Prepare email content for Withdraw single arrangement
+        if (action === "withdrawn") {
+          // Prepare email content
+          var subject1 = "WFH Request " + action;
+          var body1 = `Dear Reporting Manager (${reportingManagerEmail}),\n\n
+          A work-from-home arrangement was ${action}:\n\n
+          Staff ID: ${staffID}\n
+          Start Date: ${date}\n
+          Reason: ${reason || "N/A"}\n\n
+          Thank you. \n\n`;
+        }
+      }
+
+
+      // Send out the email
+      console.log('Subject:', subject);
+      console.log(body);
+      // Send notification using Mailtrap
+      console.log(`Email for WFH request sent successfully to ${staffEmail}.`);
+      await sendNotification(subject, body);
+      
+
+      // (for Reporting Manager Email) Send out the email to Reporting Manager for all withdrawn requests
+      if (reportingManagerEmail) {
+        console.log('Subject:', subject1);
+        console.log(body1);
+        // Send notification using Mailtrap
+        console.log(`Email for WFH request sent successfully to Reporting Manager ${reportingManagerEmail}.`);
+        await sendNotification(subject1, body1);
+      }
     }
-
-
-    // Send out the email
-    console.log('Subject:', subject);
-    console.log(body);
-    // Send notification using Mailtrap
-    console.log(`Email for WFH request sent successfully to ${staffEmail}.`);
-    await sendNotification(subject, body);
 
 
     // Release the connection back to the pool
